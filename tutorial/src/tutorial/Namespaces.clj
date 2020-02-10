0b2a7beabaad15aeef163fa9208947af74f1b405
Maximillian Arruda <dearrudam@gmail.com>
2020-02-10 01:07:14 +0000

    Add Namespaces studies

--- tutorial/src/tutorial/Namespaces.clj
+++ tutorial/src/tutorial/Namespaces.clj
@@ -1,10 +1,18 @@
 (ns tutorial.Namespaces
-    (:require [clojure.string :refer [capitalize]])
+; option 1 
+;    (:require [clojure.string])
+; option 2 
+     (:require [clojure.string :refer [capitalize]])
+; option 3
 ;    (:require [clojure.string :refer :all])
 )
 
 (defn -main
     []
-    (println (capitalize "hello")))
+; using option 1
+;    (println (clojure.string/capitalize "hello"))
+; using option 2 and 3
+    (println (capitalize "hello"))
+)
 
 (-main)
